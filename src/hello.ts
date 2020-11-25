a8b77845d2cce160c77b33aaf4fb306be4ace7f2
kflange <zknsftk1msrk@gmail.com>
2020-11-25 15:40:30 +0900

    Changed: to typescript

---
+++ src/hello.ts
@@ -0,0 +1,13 @@
+import {APIGatewayEvent, Context} from 'aws-lambda'
+
+export async function handler( event: APIGatewayEvent, context: Context) {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      msg: 'Hello netlify functions'
+    })
+  }
+}
---
+++ x.sh
@@ -0,0 +1,2 @@
+#!/usr/bin
+curl localhost:8888/.netlify/functions/handler
